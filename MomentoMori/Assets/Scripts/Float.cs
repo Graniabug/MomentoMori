a1639b60e7529e8443f3d4fb9f7cce9d88c8846d
Kayleigh Shaw <ks675713@ohio.edu>
2020-11-23 14:41:26 -0500

    wip/new game and quit menus working, continue started

---
+++ MomentoMori/Assets/Scripts/Float.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Float : MonoBehaviour
+{
+    int floatAmount;
+
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //MakeFloat();
+
+        //ScaleOnSelected();
+    }
+
+    void MakeFloat()
+    {
+        Vector3 floatUp = this.transform.position;
+        floatUp.x = floatUp.x + floatAmount;
+        floatUp.y = floatUp.y + floatAmount;
+
+        Vector3 floatDown = this.transform.position;
+        floatDown.x = floatDown.x - floatAmount;
+        floatDown.y = floatUp.y - floatAmount;
+
+        if (this.transform.position.x < floatUp.x && this.transform.position.y < floatUp.y)
+        {
+            this.transform.position = Vector3.Lerp(this.transform.position, floatUp, 0.1f);
+        }
+        else if (this.transform.position.x > floatDown.x && this.transform.position.y > floatDown.y)
+        {
+            this.transform.position = Vector3.Lerp(this.transform.position, floatDown, 0.1f);
+        }
+    }
+
+    void ScaleOnSelected()
+    {
+        //Vector3 scaleModifier = new Vector3(1.25f, 1.25f, 1.25f);
+        print(EventSystem.current);
+        if (EventSystem.current.currentSelectedGameObject == this)
+        {
+            print("is selected");
+            Vector3 selectedScale = this.transform.localScale;
+            selectedScale.x *= 1.25f;
+            selectedScale.y *= 1.25f;
+            selectedScale.z *= 1.25f;
+            this.transform.localScale = selectedScale;
+        }
+    }
+}
--- MomentoMori/Assets/Scripts/MainMenu.cs
+++ MomentoMori/Assets/Scripts/MainMenu.cs
@@ -7,16 +7,40 @@ public class MainMenu : MonoBehaviour
 
     public GameObject defaultMenuArt;
     public GameObject specialMenuArt;
+    public GameObject backGround;
+    public GameObject mainMenu;
+    public GameObject newGameMenu;
+    public GameObject continueMenu;
+    public GameObject singlePlayerMenu;
+    public GameObject multiplayerMenu;
+    public GameObject optionsMenu;
+    public GameObject quitMenu;
+
+    Vector3 leftShift;
+    Vector3 rightShift;
+    Vector3 BGOrigin;
+
+    float lerpTime = 0.2f;
 
     bool optionsPressed = false;
     bool continuePressed = false;
     bool newGamePressed = false;
-
-    //public GameObject[] buttons = new GameObject[4];
+    bool resetBGPosition = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        BGOrigin = backGround.transform.position;
+
+        backGround.SetActive(true);
+        mainMenu.SetActive(true);
+        newGameMenu.SetActive(false);
+        continueMenu.SetActive(false);
+        singlePlayerMenu.SetActive(false);
+        multiplayerMenu.SetActive(false);
+        optionsMenu.SetActive(false);
+        quitMenu.SetActive(false);
+
         //show the easter egg menu art 1/50th of the time
         //otherwise, show the default art
         int choose = Random.Range(1, 50);
@@ -33,19 +57,48 @@ public class MainMenu : MonoBehaviour
 
         //deactiveate the "Continue" button if there are no ongoing games
 
+
+        leftShift = backGround.transform.position;
+        leftShift.x = 300;
+        rightShift = backGround.transform.position;
+        rightShift.x = 1000;
     }
 
     private void Update()
     {
         //shift BG to the right if continue is pressed
-
+        if (continuePressed)
+        {
+            backGround.transform.position = Vector3.Lerp(backGround.transform.position, rightShift, lerpTime);
+            if (Vector3.Distance(backGround.transform.position, rightShift).ToString("0.00") == "0.00")
+            {
+                print("lerp done");
+                continuePressed = false;
+            }
+        }
 
         //shift BG to the left if new game is pressed
-
+        if (newGamePressed)
+        {
+            backGround.transform.position = Vector3.Lerp(backGround.transform.position, leftShift, lerpTime);
+            if (Vector3.Distance(backGround.transform.position, leftShift).ToString("0.00") == "0.00")
+            {
+                print("lerp done");
+                newGamePressed = false;
+            }
+        }
 
         //pull a grey box up from below if options is pressed
 
-
+        if (resetBGPosition)
+        {
+            backGround.transform.position = Vector3.Lerp(backGround.transform.position, BGOrigin, lerpTime);
+            if (Vector3.Distance(backGround.transform.position, BGOrigin).ToString("0.00") == "0.00")
+            {
+                print("lerp done");
+                resetBGPosition = false;
+            }
+        }
     }
 
     public void Options()
@@ -56,16 +109,68 @@ public class MainMenu : MonoBehaviour
     public void Continue()
     {
         continuePressed = true;
+        mainMenu.SetActive(false);
+        continueMenu.SetActive(true);
     }
 
     public void NewGame()
     {
         newGamePressed = true;
+        mainMenu.SetActive(false);
+        newGameMenu.SetActive(true);
+    }
+    public void OpenQuitMenu()
+    {
+        mainMenu.SetActive(false);
+        quitMenu.SetActive(true);
     }
 
     public void Quit()
     {
-        //end the game
         Application.Quit();
+    }
+
+    public void BackFromNewGame()
+    {
+        resetBGPosition = true;
+        singlePlayerMenu.SetActive(false);
+        multiplayerMenu.SetActive(false);
+        newGameMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+    public void BackFromContinue()
+    {
+        resetBGPosition = true;
+        continueMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+    public void BackFromOptions()
+    {
+
+    }
+
+    public void BackFromQuit()
+    {
+        quitMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+    public void SelectSinglePlayer()
+    {
+        multiplayerMenu.SetActive(false);
+        singlePlayerMenu.SetActive(true);
+    }
+
+    public void SelectMultiplayer()
+    {
+        singlePlayerMenu.SetActive(false);
+        multiplayerMenu.SetActive(true);
+    }
+
+    public void Play()
+    {
+
     }
 }
